d6f4d1d01eb931327e7ee54cb25a1324edf0f124
Lola <lolitasav884@gmail.com>
2022-07-23 00:32:09 +0300

    коммит через -а

---
+++ task2.py
@@ -0,0 +1,13 @@
+# 2. Напишите программу, 
+# которая на вход принимает 5 чисел 
+# и находит максимальное из них 
+
+
+numbers = input("Введите числа через пробел: ")
+num_list = numbers.split(" ")
+max_num = num_list[0]
+for i in num_list:
+    if i > max_num:
+        max_num = i
+
+print(f"Максимальное число - {max_num}")
